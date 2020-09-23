0b3928a8d690d6f5916fc757d1941929617d84d4
Muhammad Talha Khalid <38003373+MuhammadTalhaKhalid@users.noreply.github.com>
2020-09-23 08:08:40 -0700

    Add files via upload

--- apple_macintosh_lib/mac.h
+++ apple_macintosh_lib/mac.h
@@ -1,11 +1,11 @@
 #include"melody.h"
 int sout;
 int notes[7]={f5,a5,c6,f6,a5,as5,fs5};
-int Delay[6]={430,430,430,598,280,298};
+int Delay[6]={330,330,330,618,280,598};
 
 void mac_startup() {
 for(int i=0;i<=6;i++) {
- tone(sout,notes[i],500);
+ tone(sout,notes[i],700);
 delay(Delay[i]);   
 }
 
