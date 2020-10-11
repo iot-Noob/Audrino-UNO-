aef052ddfb2679ef9368166d445d6e6f6709e2cb
Muhammad Talha Khalid <38003373+MuhammadTalhaKhalid@users.noreply.github.com>
2020-10-11 07:50:13 -0700

    Factory alarm
    play factory alarm can be use as alert sound enjoy

---
+++ Factory/Factory.ino
@@ -0,0 +1,16 @@
+/*
+ * download PCM.h library for arduino
+ * use pin 11 for output of audio
+ * enjoy
+ */
+*/
+#include"factory.h"
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+ 
+fa();
+ delay(650);
+}
