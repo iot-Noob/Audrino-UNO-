9bea9d9ea538c22d59fc698560cf80217800fe32
Noob Programmer <38003373+iot-Noob@users.noreply.github.com>
2021-03-22 18:01:12 +0500

    Battery Management system library
    made a library for battery management system and display its output to LCD V1

---
+++ battery/battery.cpp
@@ -0,0 +1,56 @@
+#include"Arduino.h"
+#include"battery.h"
+#include "LiquidCrystal.h"
+ const int rs = 6, en = 7, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  
+void  battery::begin(int br,int bc) {
+
+   Serial.begin(9600);
+  lcd.begin(16, 2);
+  btr=br;
+  btc=bc;
+ }
+ battery::battery(int inp) {
+  vin=inp;
+ }
+
+ void  battery::cal_voltage() {
+sv=analogRead(vin);
+voltage= (sv * 5.0) / 1024.0; 
+ bp=(voltage/5)*100;
+
+Serial.println(voltage);
+}
+void battery::bat_stat(int bp) {
+ 
+ lcd.createChar(0,batery[bp][0]);
+lcd.createChar(1,batery[bp][1]); 
+}
+void battery::draw_battery(int r,int c) {
+ 
+ lcd.setCursor(r,c); 
+ lcd.write(byte(0));
+  lcd.setCursor(r+1,c); 
+ lcd.write(byte(1));
+ 
+
+}
+
+void  battery::charge() {
+
+  btsmap=map(bp,0,100,0,10);
+   bat_stat(btsmap); 
+     lcd.setCursor(btr+2,btc); 
+    lcd.print(bp);
+   lcd.print("%");
+ }
+
+void battery::Main() {
+   delay(800);
+  draw_battery(btr,btc);
+cal_voltage();  
+charge();
+
+
+}
---
+++ battery/battery.ino
@@ -0,0 +1,26 @@
+/*
+
+
+  The circuit:
+ * LCD RS pin to digital pin 6
+ * LCD Enable pin to digital pin 7
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+#include"battery.h"
+battery b1(A0);
+void setup() {
+  b1.begin(0,0);
+}
+
+void loop() {
+ b1.Main();
+}
