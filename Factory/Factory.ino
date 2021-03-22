514f7cebf61a551f0391d5de1745874c4c4a018b
Noob Programmer <38003373+iot-Noob@users.noreply.github.com>
2021-03-22 14:30:21 +0500

    Factory alarm PCM Update 2021
    Done some update in this library  enjoy alarm sound without need of a sd card shield or music player

--- Factory/Factory.ino
+++ Factory/Factory.ino
@@ -3,10 +3,17 @@
  * use pin 11 for output of audio
  * enjoy
  */
-*/
+
 #include"factory.h"
 void setup() {
 Serial.begin(9600);
+Serial.print(F("Initilize Alarm"));
+for(int i=0;i<=6;i++) {
+ Serial.print(F(".")); 
+ delay(400);
+}
+delay(1000);
+Serial.print(F("\nAlarm started"));
 }
 
 void loop() {
